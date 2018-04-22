5c88c0a61b611d0a236f8e1f34fb41999f1f0b9b
Alexander Petrovskiy <uiautomation@hotmail.com>
2018-04-22 23:33:52 +0300

    .

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,38 @@
+﻿// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+	using System.Text.Encodings.Web;
+
+	public class HelloWorldController : Controller
+    {
+		// GET: /HelloWorld/
+		// 1
+		// public string Index()
+		// 4
+		public IActionResult Index()
+		{
+			// 1
+			// return "This is my default action...";
+			// 4
+			return View();
+        }
+
+		// GET: /HelloWorld/Welcome/
+		// 1
+		// public string Welcome()
+		// 2
+		// public string Welcome(string name, int numTimes = 1)
+		// 3
+		public string Welcome(string name, int ID = 1)
+		{
+			// 1
+			// return "This is the Welcome action method...";
+			// 2
+			// return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+			// 3
+			return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+		}
+    }
+}
