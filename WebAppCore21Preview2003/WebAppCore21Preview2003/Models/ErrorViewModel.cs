b939330c38a4feccb85d98343cc55368be902aa2
Alexander Petrovskiy <uiautomation@hotmail.com>
2018-04-27 23:47:56 +0300

    .

---
+++ WebAppCore21Preview2003/WebAppCore21Preview2003/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppCore21Preview2003.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
